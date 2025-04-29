03748bb0afc99ec73199f148b61836c5e17d6760
AdrienGQT 🤸 <adrien.gsqt@gmail.com>
2025-04-29 17:03:27 +0200

    get and display all items

---
+++ src/js/Item.js
@@ -0,0 +1,34 @@
+export class Item{
+    constructor(payload, card){
+        console.log('Item Initialized')
+
+        this.item = payload
+        this.card = card
+
+        this.itemHTML = {}
+
+        this.init()
+    }
+
+    init = () => {
+        this.cacheHTML()
+        this.editHTML()
+    }
+
+    cacheHTML = () => {
+        this.itemHTML.title = this.card.querySelector('.itemTitle')
+        this.itemHTML.image = this.card.querySelector('.itemImage')
+        this.itemHTML.date = this.card.querySelector('.itemDate')
+        this.itemHTML.location = this.card.querySelector('.itemLocation')
+        this.itemHTML.city = this.card.querySelector('.itemCity')
+        this.itemHTML.country = this.card.querySelector('.itemCountry')
+    }
+
+    editHTML = () => {
+        this.itemHTML.image.src = this.item.image_path
+        this.itemHTML.title.innerText = this.item.title
+        this.itemHTML.date.innerText = this.item.date
+        this.itemHTML.city.innerText = this.item.locations.city
+        this.itemHTML.country.innerText = this.item.locations.country
+    }
+}
--- src/js/Museum.js
+++ src/js/Museum.js
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js"
+import { Item } from "./Item"
 
 export class Museum{
     constructor(){
-        console.log('new Museum')
+        console.log('Museum Initialized')
 
         this.supabaseUrl = 'https://ywyajigbvwupvusgujoc.supabase.co'
         this.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3eWFqaWdidnd1cHZ1c2d1am9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTYwMzMsImV4cCI6MjA2MTQ5MjAzM30.22fC8mHg7-6hBF_KcTzPMQgZZ68y8UZ9Bo8k5dCbQmc'
@@ -12,9 +13,31 @@ export class Museum{
 
     init = () => {
         this.initSupabase()
+        this.getItems()
+        this.cacheDOM()
     }
 
     initSupabase = () => {
         this.supabase = createClient(this.supabaseUrl, this.supabaseKey)
+    }
+
+    getItems = async () => {
+        const datas = await this.supabase.from('items').select('*, locations (country, city)')
+
+        datas.data.forEach(e => {
+            const itemTemplateClone = this.itemTemplate.cloneNode(true)
+            const item = new Item(e, itemTemplateClone)
+            this.displayItem(item.card)
+        });
+    }
+
+    displayItem = (html) => {
+        this.map.appendChild(html)
+    }
+
+    cacheDOM = () => {
+        this.itemTemplate = document.querySelector('.item')
+        this.itemTemplate.remove()
+        this.map = document.querySelector('body')
     }
 }
